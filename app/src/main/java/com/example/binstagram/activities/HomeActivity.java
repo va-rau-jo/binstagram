6cc7eb80b2db115025ad23672f9c25d3b3c429fe
Victor Araujo <varaujo@fb.com>
2019-07-09 12:35:56 -0700

    added feed with image, username, description, and scrolling refresh

--- app/src/main/java/com/example/binstagram/activities/HomeActivity.java
+++ app/src/main/java/com/example/binstagram/activities/HomeActivity.java
@@ -4,19 +4,23 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.binstagram.R;
+import com.example.binstagram.adapters.PostAdapter;
 import com.example.binstagram.models.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -24,6 +28,15 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ArrayList<Post> posts;
+    private PostAdapter adapter;
+
+    @BindView(R.id.swipeContainer)
+    SwipeRefreshLayout swipeContainer;
+
+    @BindView(R.id.rvPosts)
+    RecyclerView rvPosts;
 
     @BindView(R.id.btnCreatePost)
     ImageButton btnCreatePost;
@@ -48,7 +61,43 @@ public class HomeActivity extends AppCompatActivity {
         if(getActionBar() != null)
             getActionBar().setDisplayShowTitleEnabled(false);
 
+        posts = new ArrayList<>();
+        adapter = new PostAdapter(HomeActivity.this, posts);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvPosts.setLayoutManager(linearLayoutManager);
+        rvPosts.setAdapter(adapter);
+
+        // Add swiping up to refresh
+        addSwipeRefreshListener();
+
+        // Load the initial posts
         loadTopPosts();
+    }
+
+    private void addSwipeRefreshListener() {
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                final Post.Query postQuery = new Post.Query();
+                postQuery.getTop().withUser();
+
+                postQuery.findInBackground(new FindCallback<Post>() {
+                    @Override
+                    public void done(List<Post> objects, ParseException e) {
+                        if(e == null) {
+                            adapter.clear();
+                            for(int i = objects.size() - 1; i >= 0; i--) {
+                                posts.add(objects.get(i));
+                                adapter.notifyItemInserted(posts.size() - 1);
+                                swipeContainer.setRefreshing(false);
+                            }
+                        } else {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     private void loadTopPosts() {
@@ -59,8 +108,10 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void done(List<Post> objects, ParseException e) {
                 if(e == null) {
-                    for(int i = 0; i < objects.size(); i++)
-                        Log.d("HomeAcitivyt", objects.get(i).getDescription());
+                    for(int i = objects.size() - 1; i >= 0; i--) {
+                        posts.add(objects.get(i));
+                        adapter.notifyItemInserted(posts.size() - 1);
+                    }
                 } else {
                     e.printStackTrace();
                 }
---
+++ app/src/main/java/com/example/binstagram/adapters/PostAdapter.java
@@ -0,0 +1,96 @@
+package com.example.binstagram.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.binstagram.R;
+import com.example.binstagram.models.Post;
+import com.parse.ParseImageView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+
+    private ArrayList<Post> posts;
+    private Context context;
+    private Activity activity;
+
+    public PostAdapter(Activity context, ArrayList<Post> posts) {
+        this.posts = posts;
+        this.activity = context;
+    }
+
+    /**
+     * Method called when the adapter has to display the data on a view, creates one view holder
+     * @param viewGroup The parent view
+     * @param viewType Type of the parent view, doesn't matter here
+     * @return The created view holder
+     */
+    @NonNull
+    @Override
+    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View tweetView = layoutInflater.inflate(R.layout.item_post, viewGroup, false);
+        return new ViewHolder(tweetView);
+    }
+
+    /**
+     * Method called to bind data to an already created view holder
+     * @param viewHolder The already created view holder that will be populated
+     * @param index The index of the view holder
+     */
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder viewHolder, int index) {
+        Post post = posts.get(index);
+        viewHolder.tvUsername.setText(post.getUser().getUsername());
+        viewHolder.tvUsernameLabel.setText(post.getUser().getUsername());
+        viewHolder.tvDescription.setText(post.getDescription());
+
+        viewHolder.ivImage.setParseFile(post.getImage());
+        viewHolder.ivImage.loadInBackground();
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    /**
+     * Helper method to clear the list of posts, so we can refresh
+     */
+    public void clear() {
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivImage)
+        ParseImageView ivImage;
+
+        @BindView(R.id.tvUsername)
+        TextView tvUsername;
+
+        @BindView(R.id.tvDescription)
+        TextView tvDescription;
+
+        // The label next to the description
+        @BindView(R.id.tvUsernameLabel)
+        TextView tvUsernameLabel;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
